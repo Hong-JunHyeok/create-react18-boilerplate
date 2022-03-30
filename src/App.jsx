a3a071845f669088c49e65b9b30c8a1e5d83926c
Hong-JunHyeok <edb1631@dgsw.hs.kr>
2022-03-30 09:32:47 +0900

    ADD Test code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-const React = require("react");
+import React from "react";
 
 class App extends React.Component {
+  state = {
+    version: 18,
+  };
+
+  handleIncreaseVersion = () => {
+    this.setState({
+      ...this.state,
+      version: this.state.version + 1,
+    });
+  };
+
   render() {
-    return <h1>React 18 BoilerPlate</h1>;
+    return (
+      <h1 onClick={this.handleIncreaseVersion}>
+        React {this.state.version} BoilerPlate
+      </h1>
+    );
   }
 }
 

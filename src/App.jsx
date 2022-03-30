3588939eb8389963a9d7f271a32c12a95a989b2b
Hong-JunHyeok <edb1631@dgsw.hs.kr>
2022-03-30 09:29:05 +0900

    ADD App component rendering

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const React = require("react");
+
+class App extends React.Component {
+  render() {
+    return <h1>React 18 BoilerPlate</h1>;
+  }
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,37 @@
 const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   name: "React-18_Boiler-Plate",
   mode: "development",
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-  entry: {
-    app: ["./src/index.jsx"],
+  entry: "./src/index.jsx",
+  output: {
+    filename: "bundle.[hash].js",
+    path: path.resolve("dist"),
+    publicPath: "/",
   },
   module: {
     rules: [
       {
-        test: /\.jsx?/,
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
-        },
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
       },
     ],
   },
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "app.js",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 3080,
   },
 };

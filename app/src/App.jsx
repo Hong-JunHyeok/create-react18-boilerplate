ff49260a22257b964843481bdd5cb352890456e9
Hong-JunHyeok <edb1631@dgsw.hs.kr>
2022-04-02 13:14:43 +0900

    FIX file copy error

---
+++ app/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return <>React 18 BoilerPlate</>;
+  }
+}
+
+export default App;
---
+++ app/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  name: "React-18_Boiler-Plate",
+  mode: "development",
+  entry: "./src/index.jsx",
+  output: {
+    filename: "bundle.[chunkhash].js",
+    path: path.resolve("dist"),
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "images/[name].[ext]?[chunkhash]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 3080,
+    open: true,
+  },
+};
